536911ea1aa4309794b78835049228019f08e863
Tandi Samples <tandisamples@gmail.com>
2019-12-02 19:34:10 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :shows, through: :characters
+  has_many :characters
+
+  def full_name
+    name = "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles = []
+    self.characters.each(){|character|
+      roles << "#{character.name} - #{character.show.name}"
+    }
+    roles
+  end
 end
